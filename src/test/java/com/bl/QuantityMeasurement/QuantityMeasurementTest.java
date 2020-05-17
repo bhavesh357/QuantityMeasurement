d84260fbaa7026ba9b586ea20bdfca427ee2feaf
Bhavesh Kadam <bkadam357@gmail.com>
2020-05-17 21:51:36 +0530

    MODIFIED: variable names

--- src/test/java/com/bl/QuantityMeasurement/QuantityMeasurementTest.java
+++ src/test/java/com/bl/QuantityMeasurement/QuantityMeasurementTest.java
@@ -20,40 +20,40 @@ public class QuantityMeasurementTest {
 
     @Test
     public void givenFeetAndFeet_WhenFirstNull_ShouldReturnFalse() {
-        Feet one= new Feet(0);
-        Feet two = null;
-        boolean isEqual = quantityMeasurement.equalComparator(two, one);
+        Feet firstUnit= new Feet(0);
+        Feet secondUnit = null;
+        boolean isEqual = quantityMeasurement.equalComparator(secondUnit, firstUnit);
         Assert.assertFalse(isEqual);
     }
 
     @Test
     public void givenFeetAndFeet_WhenSecondNull_ShouldReturnFalse() {
-        Feet one= new Feet(0);
-        Feet two = null;
-        boolean isEqual = quantityMeasurement.equalComparator(one, two);
+        Feet firstUnit= new Feet(0);
+        Feet secondUnit = null;
+        boolean isEqual = quantityMeasurement.equalComparator(firstUnit, secondUnit);
         Assert.assertFalse(isEqual);
     }
 
     @Test
     public void givenFeetAndFeet_WhenSame_ShouldReturnTrue() {
-        Feet one= new Feet(0);
-        boolean isEqual = quantityMeasurement.equalComparator(one, one);
+        Feet firstUnit= new Feet(0);
+        boolean isEqual = quantityMeasurement.equalComparator(firstUnit, firstUnit);
         Assert.assertTrue(isEqual);
     }
 
     @Test
     public void givenFeetAndFeet_WhenTypeSame_ShouldReturnTrue() {
-        Feet one= new Feet(0);
-        Feet two = new Feet(0);
-        boolean isEqual = quantityMeasurement.equalComparator(one, two);
+        Feet firstUnit= new Feet(0);
+        Feet secondUnit = new Feet(0);
+        boolean isEqual = quantityMeasurement.equalComparator(firstUnit, secondUnit);
         Assert.assertTrue(isEqual);
     }
 
     @Test
     public void givenFeetAndFeet_WhenDiffLength_ShouldReturnFalse() {
-        Feet one= new Feet(0);
-        Feet two = new Feet(10);
-        boolean isEqual = quantityMeasurement.equalComparator(one, two);
+        Feet firstUnit= new Feet(0);
+        Feet secondUnit = new Feet(10);
+        boolean isEqual = quantityMeasurement.equalComparator(firstUnit, secondUnit);
         Assert.assertFalse(isEqual);
     }
 
@@ -65,232 +65,232 @@ public class QuantityMeasurementTest {
 
     @Test
     public void givenInchAndInch_WhenFirstNull_ShouldReturnFalse() {
-        Inch one= new Inch(0);
-        Inch two = null;
-        boolean isEqual = quantityMeasurement.equalComparator(two, one);
+        Inch firstUnit= new Inch(0);
+        Inch secondUnit = null;
+        boolean isEqual = quantityMeasurement.equalComparator(secondUnit, firstUnit);
         Assert.assertFalse(isEqual);
     }
 
     @Test
     public void givenInchAndInch_WhenSecondNull_ShouldReturnFalse() {
-        Inch one= new Inch(0);
-        Inch two = null;
-        boolean isEqual = quantityMeasurement.equalComparator(one, two);
+        Inch firstUnit= new Inch(0);
+        Inch secondUnit = null;
+        boolean isEqual = quantityMeasurement.equalComparator(firstUnit, secondUnit);
         Assert.assertFalse(isEqual);
     }
 
     @Test
     public void givenInchAndInch_WhenSame_ShouldReturnTrue() {
-        Inch one= new Inch(0);
-        boolean isEqual = quantityMeasurement.equalComparator(one, one);
+        Inch firstUnit= new Inch(0);
+        boolean isEqual = quantityMeasurement.equalComparator(firstUnit, firstUnit);
         Assert.assertTrue(isEqual);
     }
 
     @Test
     public void givenInchAndInch_WhenTypeSame_ShouldReturnTrue() {
-        Inch one= new Inch(0);
-        Inch two = new Inch(0);
-        boolean isEqual = quantityMeasurement.equalComparator(one, two);
+        Inch firstUnit= new Inch(0);
+        Inch secondUnit = new Inch(0);
+        boolean isEqual = quantityMeasurement.equalComparator(firstUnit, secondUnit);
         Assert.assertTrue(isEqual);
     }
 
     @Test
     public void givenInchAndInch_WhenDiffLength_ShouldReturnFalse() {
-        Inch one= new Inch(0);
-        Inch two = new Inch(10);
-        boolean isEqual = quantityMeasurement.equalComparator(one, two);
+        Inch firstUnit= new Inch(0);
+        Inch secondUnit = new Inch(10);
+        boolean isEqual = quantityMeasurement.equalComparator(firstUnit, secondUnit);
         Assert.assertFalse(isEqual);
     }
 
     @Test
     public void givenFeetAndInch_WhenZero_ShouldReturnTrue() {
-        Feet one= new Feet(0);
-        Inch two = new Inch(0);
-        boolean isEqual = quantityMeasurement.equalComparator(one, two);
+        Feet firstUnit= new Feet(0);
+        Inch secondUnit = new Inch(0);
+        boolean isEqual = quantityMeasurement.equalComparator(firstUnit, secondUnit);
         Assert.assertTrue(isEqual);
     }
 
     @Test
-    public void givenInchAndFeet_WhenOne_ShouldReturnFalse() {
-        Inch two = new Inch(1);
-        Feet one= new Feet(1);
-        boolean isEqual = quantityMeasurement.equalComparator(one, two);
+    public void givenInchAndFeet_WhenfirstUnit_ShouldReturnFalse() {
+        Inch secondUnit = new Inch(1);
+        Feet firstUnit= new Feet(1);
+        boolean isEqual = quantityMeasurement.equalComparator(firstUnit, secondUnit);
         Assert.assertFalse(isEqual);
     }
 
     @Test
-    public void givenFeetAndInch_WhenOne_ShouldReturnFalse() {
-        Inch two = new Inch(1);
-        Feet one= new Feet(1);
-        boolean isEqual = quantityMeasurement.equalComparator(two, one);
+    public void givenFeetAndInch_WhenfirstUnit_ShouldReturnFalse() {
+        Inch secondUnit = new Inch(1);
+        Feet firstUnit= new Feet(1);
+        boolean isEqual = quantityMeasurement.equalComparator(secondUnit, firstUnit);
         Assert.assertFalse(isEqual);
     }
 
     @Test
     public void givenFeetAndInch_WhenProper_ShouldReturnTrue() {
-        Feet one= new Feet(1);
-        Inch two = new Inch(12);
-        boolean isEqual = quantityMeasurement.equalComparator(one, two);
+        Feet firstUnit= new Feet(1);
+        Inch secondUnit = new Inch(12);
+        boolean isEqual = quantityMeasurement.equalComparator(firstUnit, secondUnit);
         Assert.assertTrue(isEqual);
     }
 
     @Test
     public void givenInchAndFeet_WhenProper_ShouldReturnTrue() {
-        Inch two = new Inch(12);
-        Feet one= new Feet(1);
-        boolean isEqual = quantityMeasurement.equalComparator(two, one);
+        Inch secondUnit = new Inch(12);
+        Feet firstUnit= new Feet(1);
+        boolean isEqual = quantityMeasurement.equalComparator(secondUnit, firstUnit);
         Assert.assertTrue(isEqual);
     }
 
     @Test
     public void givenFeetAndYard_WhenProper_ShouldReturnTrue() {
-        Yard two = new Yard(1);
-        Feet one= new Feet(3);
-        boolean isEqual = quantityMeasurement.equalComparator(one, two);
+        Yard secondUnit = new Yard(1);
+        Feet firstUnit= new Feet(3);
+        boolean isEqual = quantityMeasurement.equalComparator(firstUnit, secondUnit);
         Assert.assertTrue(isEqual);
     }
 
     @Test
-    public void givenFeetAndYard_WhenOne_ShouldReturnFalse() {
-        Yard two = new Yard(1);
-        Feet one= new Feet(1);
-        boolean isEqual = quantityMeasurement.equalComparator(one, two);
+    public void givenFeetAndYard_WhenfirstUnit_ShouldReturnFalse() {
+        Yard secondUnit = new Yard(1);
+        Feet firstUnit= new Feet(1);
+        boolean isEqual = quantityMeasurement.equalComparator(firstUnit, secondUnit);
         Assert.assertFalse(isEqual);
     }
 
     @Test
-    public void givenInchAndYard_WhenOne_ShouldReturnFalse() {
-        Yard two = new Yard(1);
-        Inch one= new Inch(1);
-        boolean isEqual = quantityMeasurement.equalComparator(one, two);
+    public void givenInchAndYard_WhenfirstUnit_ShouldReturnFalse() {
+        Yard secondUnit = new Yard(1);
+        Inch firstUnit= new Inch(1);
+        boolean isEqual = quantityMeasurement.equalComparator(firstUnit, secondUnit);
         Assert.assertFalse(isEqual);
     }
 
     @Test
     public void givenYardAndInch_WhenProper_ShouldReturnTrue() {
-        Yard one = new Yard(1);
-        Inch two= new Inch(36);
-        boolean isEqual = quantityMeasurement.equalComparator(one, two);
+        Yard firstUnit = new Yard(1);
+        Inch secondUnit= new Inch(36);
+        boolean isEqual = quantityMeasurement.equalComparator(firstUnit, secondUnit);
         Assert.assertTrue(isEqual);
     }
 
     @Test
     public void givenInchAndYard_WhenProper_ShouldReturnTrue() {
-        Inch one= new Inch(36);
-        Yard two = new Yard(1);
-        boolean isEqual = quantityMeasurement.equalComparator(one, two);
+        Inch firstUnit= new Inch(36);
+        Yard secondUnit = new Yard(1);
+        boolean isEqual = quantityMeasurement.equalComparator(firstUnit, secondUnit);
         Assert.assertTrue(isEqual);
     }
 
     @Test
     public void givenYardAndFeet_WhenProper_ShouldReturnTrue() {
-        Yard one = new Yard(1);
-        Feet two= new Feet(3);
-        boolean isEqual = quantityMeasurement.equalComparator(one, two);
+        Yard firstUnit = new Yard(1);
+        Feet secondUnit= new Feet(3);
+        boolean isEqual = quantityMeasurement.equalComparator(firstUnit, secondUnit);
         Assert.assertTrue(isEqual);
     }
 
     @Test
     public void givenInchAndCm_WhenProper_ShouldReturnTrue() {
-        Inch one = new Inch(2);
-        Centimeters two= new Centimeters(5);
-        boolean isEqual = quantityMeasurement.equalComparator(one, two);
+        Inch firstUnit = new Inch(2);
+        Centimeters secondUnit= new Centimeters(5);
+        boolean isEqual = quantityMeasurement.equalComparator(firstUnit, secondUnit);
         Assert.assertTrue(isEqual);
     }
 
     @Test
     public void givenInchAndInch_WhenProper_ShouldReturnAddition() {
-        Inch one = new Inch(2);
-        Inch two= new Inch(2);
-        double addition = quantityMeasurement.addLength(one, two);
+        Inch firstUnit = new Inch(2);
+        Inch secondUnit= new Inch(2);
+        double addition = quantityMeasurement.addLength(firstUnit, secondUnit);
         Assert.assertEquals(4,addition,0.0);
     }
 
     @Test
     public void givenFeetAndInch_WhenProper_ShouldReturnAddition() {
-        Feet one = new Feet(1);
-        Inch two= new Inch(2);
-        double addition = quantityMeasurement.addLength(one, two);
+        Feet firstUnit = new Feet(1);
+        Inch secondUnit= new Inch(2);
+        double addition = quantityMeasurement.addLength(firstUnit, secondUnit);
         Assert.assertEquals(14,addition,0.0);
     }
 
     @Test
     public void givenFeetAndFeet_WhenProper_ShouldReturnAddition() {
-        Feet one = new Feet(1);
-        Feet two= new Feet(1);
-        double addition = quantityMeasurement.addLength(one, two);
+        Feet firstUnit = new Feet(1);
+        Feet secondUnit= new Feet(1);
+        double addition = quantityMeasurement.addLength(firstUnit, secondUnit);
         Assert.assertEquals(24,addition,0.0);
     }
 
     @Test
     public void givenInchAndCm_WhenProper_ShouldReturnAddition() {
-        Inch one = new Inch(2);
-        Centimeters two= new Centimeters(2.5);
-        double addition = quantityMeasurement.addLength(one, two);
+        Inch firstUnit = new Inch(2);
+        Centimeters secondUnit= new Centimeters(2.5);
+        double addition = quantityMeasurement.addLength(firstUnit, secondUnit);
         Assert.assertEquals(3,addition,0.0);
     }
 
     @Test
     public void givenGallonAndLitre_WhenProper_ShouldReturnTrue() {
-        Gallon one = new Gallon(1);
-        Litre two = new Litre(3.78);
-        boolean isEqual = quantityMeasurement.equalComparator(one,two);
+        Gallon firstUnit = new Gallon(1);
+        Litre secondUnit = new Litre(3.78);
+        boolean isEqual = quantityMeasurement.equalComparator(firstUnit,secondUnit);
         Assert.assertTrue(isEqual);
     }
 
     @Test
     public void givenLitreAndMl_WhenProper_ShouldReturnTrue() {
-        Litre one = new Litre(1);
-        Mililitre two = new Mililitre(1000);
-        boolean isEqual = quantityMeasurement.equalComparator(one,two);
+        Litre firstUnit = new Litre(1);
+        Mililitre secondUnit = new Mililitre(1000);
+        boolean isEqual = quantityMeasurement.equalComparator(firstUnit,secondUnit);
         Assert.assertTrue(isEqual);
     }
 
     @Test
     public void givenGallonAndLitre_WhenProper_ShouldReturnAddition() {
-        Gallon one = new Gallon(1);
-        Litre two= new Litre(3.78);
-        double addition = quantityMeasurement.addVolume(one, two);
+        Gallon firstUnit = new Gallon(1);
+        Litre secondUnit= new Litre(3.78);
+        double addition = quantityMeasurement.addVolume(firstUnit, secondUnit);
         Assert.assertEquals(7.57,addition,0.03);
     }
 
     @Test
     public void givenLitreAndMl_WhenProper_ShouldReturnAddition() {
-        Mililitre two = new Mililitre(1000);
-        Litre one= new Litre(1);
-        double addition = quantityMeasurement.addVolume(one, two);
+        Mililitre secondUnit = new Mililitre(1000);
+        Litre firstUnit= new Litre(1);
+        double addition = quantityMeasurement.addVolume(firstUnit, secondUnit);
         Assert.assertEquals(2,addition,0.03);
     }
 
     @Test
     public void givenKgAndGram_WhenProper_ShouldReturnTrue() {
-        Kilogram one = new Kilogram(1);
-        Gram two = new Gram(1000);
-        boolean isEqual = quantityMeasurement.equalComparator(one,two);
+        Kilogram firstUnit = new Kilogram(1);
+        Gram secondUnit = new Gram(1000);
+        boolean isEqual = quantityMeasurement.equalComparator(firstUnit,secondUnit);
         Assert.assertTrue(isEqual);
     }
 
     @Test
     public void givenTonAndKg_WhenProper_ShouldReturnTrue() {
-        Ton one = new Ton(1);
-        Kilogram two = new Kilogram(1000);
-        boolean isEqual = quantityMeasurement.equalComparator(one,two);
+        Ton firstUnit = new Ton(1);
+        Kilogram secondUnit = new Kilogram(1000);
+        boolean isEqual = quantityMeasurement.equalComparator(firstUnit,secondUnit);
         Assert.assertTrue(isEqual);
     }
 
     @Test
     public void givenTonAndGram_WhenProper_ShouldReturnAddition() {
-        Ton one = new Ton(1);
-        Gram two= new Gram(1000);
-        double addition = quantityMeasurement.addWeight(one, two);
+        Ton firstUnit = new Ton(1);
+        Gram secondUnit= new Gram(1000);
+        double addition = quantityMeasurement.addWeight(firstUnit, secondUnit);
         Assert.assertEquals(1001,addition,0.03);
     }
 
     @Test
     public void givenFarenheitAndCelcius_WhenProper_ShouldReturnTrue() {
-        Farenheit one = new Farenheit(212);
-        Celcius two = new Celcius(100);
-        boolean isEqual = quantityMeasurement.equalComparator(one,two);
+        Farenheit firstUnit = new Farenheit(212);
+        Celcius secondUnit = new Celcius(100);
+        boolean isEqual = quantityMeasurement.equalComparator(firstUnit,secondUnit);
         Assert.assertTrue(isEqual);
     }
 }
